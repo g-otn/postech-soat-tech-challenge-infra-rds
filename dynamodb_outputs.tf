b6087908d01ce1c85255ac68816b7687ebd7f7fd
g-otn <44736064+g-otn@users.noreply.github.com>
2024-03-25 23:57:48 -0300

    chore: update providers

--- dynamodb_outputs.tf
+++ dynamodb_outputs.tf
@@ -1,1 +1,19 @@
+output "dynamodb_identification_svc_status_table" {
+  description = "DynamoDB Identification Service Clients Table"
+  value = {
+    "arn" : aws_dynamodb_table.identification_svc_clients_table.arn,
+    "id" : aws_dynamodb_table.identification_svc_clients_table.id,
+    "name" : aws_dynamodb_table.identification_svc_clients_table.name,
+    "tags" : aws_dynamodb_table.identification_svc_clients_table.tags,
+  }
+}
 
+output "dynamodb_production_svc_status_table" {
+  description = "DynamoDB Production Service Status Table"
+  value = {
+    "arn" : aws_dynamodb_table.production_svc_status_table.arn,
+    "id" : aws_dynamodb_table.production_svc_status_table.id,
+    "name" : aws_dynamodb_table.production_svc_status_table.name,
+    "tags" : aws_dynamodb_table.production_svc_status_table.tags,
+  }
+}
